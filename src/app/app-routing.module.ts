124d4f044449a46d8d2c8c879d4be7269d79b859
Roberto Aleydon <aleydonroberto@gmail.com>
2023-04-14 23:47:11 -0300

    Added routes home and products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsCrudComponent } from './pages/products-crud/products-crud.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'products',
+    component: ProductsCrudComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
