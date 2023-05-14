b6c0ca8fa3ca3107a6a07936c9c766d71d4f7e48
Roberto Aleydon <aleydonroberto@gmail.com>
2023-05-14 15:54:00 -0300

    Create navigate to product-create funtion

--- src/app/pages/products-crud/products-crud.component.ts
+++ src/app/pages/products-crud/products-crud.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-products-crud',
   templateUrl: './products-crud.component.html',
   styleUrls: ['./products-crud.component.css'],
 })
-export class ProductsCrudComponent {}
+export class ProductsCrudComponent {
+  constructor(private router: Router) {}
+
+  navigateToProductCreate(): void {
+    this.router.navigate(['/products/create']);
+  }
+}
