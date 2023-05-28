be0944cb331d8f5c270975f1915b50e4684dbde0
Roberto Aleydon <aleydonroberto@gmail.com>
2023-05-28 14:08:07 -0300

    Created Method to Create Product

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css'],
 })
-export class ProductCreateComponent {}
+export class ProductCreateComponent {
+  constructor(private matSnackBar: ProductService) {}
+
+  createProduct(): void {
+    this.matSnackBar.showMessage('Product Created');
+  }
+
+  cancelProductCreate(): void {
+    this.matSnackBar.showMessage('Cancel...');
+  }
+}
